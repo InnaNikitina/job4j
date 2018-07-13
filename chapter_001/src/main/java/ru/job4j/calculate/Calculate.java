effe043e4f3a13425795d2d5849efa9d376be3c7
InnaNikitina <mailto:janet-55@mail.ru>
2018-07-13 20:44:54 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculate;
 
+/**
+* @author Inna Nikitina (mailto:janet-55@mail.ru)
+* @version 1
+* @since 13.07.2018
+*/
+
 public class Calculate{
 	
 		public static void main(String[] args){
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,6 @@
+/**
+* @author Inna Nikitina (mailto:janet-55@mail.ru)
+* @version 1
+* @since 13.07.2018
+*/
+package ru.job4j.calculate;
