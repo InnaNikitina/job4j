8689369f3b4e6a369f744540c1c71b59fbdf8ba4
InnaNikitina <mailto:janet-55@mail.ru>
2018-07-23 12:42:38 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.max;
+
+/**
+ * Максимум из двух чисел.
+ *
+ * @author Inna Nikitina (mailto:janet-55@mail.ru)
+ * @since 23.07.2018
+ */
+
+public class Max {
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
